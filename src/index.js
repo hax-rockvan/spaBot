82f50f55c35df44fc30744fbf083947adcb94180
hax.rockvan <81225919+hax-rockvan@users.noreply.github.com>
2021-06-14 23:01:04 +0000

    removed env, added replit tokens, added express package, updated index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const { Client } = require("discord.js");
 const { registerCommands, registerEvents } = require("./utils/registry");
 /* const config = require("../slappey.json"); */
 const client = new Client();
-require("dotenv").config();
+const bottoken = process.env['BOTTOKEN']
+const prefix = process.env['PREFIX']
+const keepAlive = require('./server.js');
 
-(async () => {
+async () => {
   client.commands = new Map();
   client.events = new Map();
-  client.prefix = process.env.PREFIX;
+  client.prefix = process.env.prefix;
   await registerCommands(client, "../commands");
   await registerEvents(client, "../events");
-  await client.login(process.env.BOTTOKEN);
-})();
+  keepAlive();
+  await client.login(process.env.bottoken);
+
+  console.log(prefix)
+};
+
