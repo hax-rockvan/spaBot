080c3672162ec4d971e1709485aeeb07df6c73d1
hax-rockvan <hax-rockvan@gmail.com>
2021-06-15 23:38:01 +0800

    update

--- src/commands/help/MultiaccountCommand.js
+++ src/commands/help/MultiaccountCommand.js
@@ -1,17 +1,18 @@
-const BaseCommand = require("../../utils/structures/BaseCommand");
-const Discord = require("discord.js");
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require('discord.js');
 
 module.exports = class MultiaccountCommand extends BaseCommand {
   constructor() {
-    super("multiaccount", "help", []);
+    super('multiaccount', 'help', []);
   }
 
   async run(client, message, args) {
     const multiaccount = new Discord.MessageEmbed()
-      .setColor("#ffff3f")
-      .setTitle("Multi Account")
+      .setColor('#ffff3f')
+      .setTitle('Multi Account')
+      .setThumbnail('https://miro.medium.com/max/2200/0*MSmp3gLEbtHbMloT.png')
       .setImage(
-        "https://cdn.discordapp.com/attachments/458900645970313216/853283460793499668/attach.png"
+        'https://cdn.discordapp.com/attachments/458900645970313216/853283460793499668/attach.png'
       );
     message.channel.send(multiaccount);
   }
